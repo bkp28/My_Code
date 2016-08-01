393904cf53f6443694841af0e4559b5804bf34a1
Bhavik_1992 <bnpandya1992@gmail.com>
2016-08-01 22:31:40 +0530

    Initial Commit

---
+++ Practice/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Practice
+//
+//  Created by Bhavik pandya on 10/05/1938 Saka.
+//  Copyright © 1938 Saka Bhavik pandya. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
